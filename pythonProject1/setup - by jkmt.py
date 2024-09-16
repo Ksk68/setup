04e5344bb725278b2c1c5acd4e6a92b4ffa52208
Raul Viela <132754495+Ksk68@users.noreply.github.com>
2024-09-16 22:46:58 +0100

    a
    a

--- pythonProject1/setup - by jkmt.py
+++ pythonProject1/setup - by jkmt.py
@@ -1,74 +1,75 @@
+import os
 
-
-
-def banner():
-    print("""███████╗███████╗████████╗██╗   ██╗██████╗ 
+banner = """
+    ███████╗███████╗████████╗██╗   ██╗██████╗ 
     ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
     ███████╗█████╗     ██║   ██║   ██║██████╔╝
     ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
     ███████║███████╗   ██║   ╚██████╔╝██║     
     ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     
-    """)
+    """
 
-def adicionar_ficheiro(resposta,ficheiro):
+def info_maker(quantidade:int,palavras:str):
+    palavras = palavras.split(",")
+    tamanho = ""
+
+    print("         ╔════(1) " + palavras[0])
+    print("         ║")
+
+    for i in range(1, len(palavras) - 1):
+        print(f"         ╠════{tamanho + "═"}({i + 1}) {palavras[i]}")
+        print("         ║")
+        tamanho = tamanho + "══"
+
+    print(f"         ╚═══╦═════{tamanho}({len(palavras)}) {palavras[-1]}")
+    print("             ║")
+    return "             ╚═══>"
+
+
+
+def limpar():
+    os.system('cls')
+    print(banner)
+
+def adicionar_ficheiro(resposta:int,ficheiro:str):
+    pass
+def remover_ficheiro(resposta:int,ficheiro:str):
     pass
 
-def remover_ficheiro(resposta,ficheiro):
+def abrir_apps(resposta:int,ficheiro:str):
     pass
 
-def abrir_apps(resposta,ficheiro):
-    pass
-
-def add_remove(resposta,ficheiro):
+def add_remove(resposta:int,ficheiro:str):
     if (resposta == 1):
-        adicionar_ficheiro()
-    elif (resposta == 2):
-        remover_ficheiro()
-    elif (resposta == 3):
         abrir_apps()
+    elif (resposta == 2):
+        adicionar_ficheiro()
+    elif (resposta == 3):
+        remover_ficheiro()
 
-def clear():
-    try:
-        import os
-        lines = os.get_terminal_size().lines
-    except AttributeError:
-        lines = 130
-    print("\n" * lines)
 
-banner()
-resposta = int(input("""                                        
-         ╔════(1) setup jogos                             
-         ║
-         ╚══╦═════(2) setup programação
-            ║
-            ╚═══> """))
-
+print(banner)
+resposta = int(input(info_maker(3,"Setup gaming,Setup programing,Criar setup")))
 
 if(resposta == 1):
-    print("??")
+    limpar()
+    resposta = int(input(info_maker(3,"Abrir,Adicionar,Remover")))
+    add_remove(resposta, "a")
+
 elif(resposta == 2):
-    clear()
-    resposta = int(input("""                                        
-             ╔════(1) default                   
-             ║
-             ╚══╦═════(2) python
-                ║
-                ╚═══> """))
+    limpar()
+
+    resposta = int(input(info_maker(2,"Default,python")))
 
     if(resposta == 1):
-        resposta = int(input("""                                        
-                     ╔════(1) adicionar                   
-                     ║
-                     ╚══╦═════(2) remover
-                        ║
-                        ╚═══> """))
+        limpar()
+
+        resposta = int(input(info_maker(3,"Abrir,Adicionar,Remover")))
         add_remove(resposta,"a")
+
     elif(resposta == 2):
-        resposta = int(input("""                                        
-                             ╔════(1) adicionar                   
-                             ║
-                             ╚══╦═════(2) remover
-                                ║
-                                ╚═══> """))
+        limpar()
+
+        resposta = int(input(info_maker(3,"Abrir,Adicionar,Remover")))
         add_remove(resposta,"a")
 
