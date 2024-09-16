46762d0d3ce5e18fa5a3e987703c7188813bd44a
Raul Viela <132754495+Ksk68@users.noreply.github.com>
2024-09-16 08:04:11 +0100

    s
    s

--- pythonProject1/setup - by jkmt.py
+++ pythonProject1/setup - by jkmt.py
@@ -1,9 +1,74 @@
-print("""███████╗███████╗████████╗██╗   ██╗██████╗ 
-██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
-███████╗█████╗     ██║   ██║   ██║██████╔╝
-╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
-███████║███████╗   ██║   ╚██████╔╝██║     
-╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     
-                                          """)
 
-resposta = input("")
+
+
+def banner():
+    print("""███████╗███████╗████████╗██╗   ██╗██████╗ 
+    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+    ███████╗█████╗     ██║   ██║   ██║██████╔╝
+    ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
+    ███████║███████╗   ██║   ╚██████╔╝██║     
+    ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     
+    """)
+
+def adicionar_ficheiro(resposta,ficheiro):
+    pass
+
+def remover_ficheiro(resposta,ficheiro):
+    pass
+
+def abrir_apps(resposta,ficheiro):
+    pass
+
+def add_remove(resposta,ficheiro):
+    if (resposta == 1):
+        adicionar_ficheiro()
+    elif (resposta == 2):
+        remover_ficheiro()
+    elif (resposta == 3):
+        abrir_apps()
+
+def clear():
+    try:
+        import os
+        lines = os.get_terminal_size().lines
+    except AttributeError:
+        lines = 130
+    print("\n" * lines)
+
+banner()
+resposta = int(input("""                                        
+         ╔════(1) setup jogos                             
+         ║
+         ╚══╦═════(2) setup programação
+            ║
+            ╚═══> """))
+
+
+if(resposta == 1):
+    print("??")
+elif(resposta == 2):
+    clear()
+    resposta = int(input("""                                        
+             ╔════(1) default                   
+             ║
+             ╚══╦═════(2) python
+                ║
+                ╚═══> """))
+
+    if(resposta == 1):
+        resposta = int(input("""                                        
+                     ╔════(1) adicionar                   
+                     ║
+                     ╚══╦═════(2) remover
+                        ║
+                        ╚═══> """))
+        add_remove(resposta,"a")
+    elif(resposta == 2):
+        resposta = int(input("""                                        
+                             ╔════(1) adicionar                   
+                             ║
+                             ╚══╦═════(2) remover
+                                ║
+                                ╚═══> """))
+        add_remove(resposta,"a")
+
