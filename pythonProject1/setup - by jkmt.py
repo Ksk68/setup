c1c0af093688df513e550a18a630b46d51f614eb
Raul Viela <132754495+Ksk68@users.noreply.github.com>
2024-09-20 07:47:48 +0100

    a
    a

--- pythonProject1/setup - by jkmt.py
+++ pythonProject1/setup - by jkmt.py
@@ -13,6 +13,12 @@ def info_maker(quantidade:int,palavras:str):
     palavras = palavras.split(",")
     tamanho = ""
 
+    if quantidade == 1:
+        print(f"""
+           ╔════(1){palavras[0]}
+           ╚═══╗""")
+        return "               ╚═══>"
+
     print("         ╔════(1) " + palavras[0])
     print("         ║")
 
@@ -31,45 +37,75 @@ def limpar():
     os.system('cls')
     print(banner)
 
-def adicionar_ficheiro(resposta:int,ficheiro:str):
-    pass
-def remover_ficheiro(resposta:int,ficheiro:str):
-    pass
-
-def abrir_apps(resposta:int,ficheiro:str):
-    pass
-
 def add_remove(resposta:int,ficheiro:str):
-    if (resposta == 1):
-        abrir_apps()
-    elif (resposta == 2):
-        adicionar_ficheiro()
-    elif (resposta == 3):
-        remover_ficheiro()
+    if (resposta == 1):#abrir
+        abrir_apps(ficheiro)
+    elif (resposta == 2):#editar
+        os.startfile(fr"{ficheiro}")
 
+
+
+def abrir_apps(ficheiro:str):
+
+    with open(ficheiro, 'r', encoding='utf-8') as arquivo:
+        print(f"\n                  Abrindo arquivo: {ficheiro}")
+        while True:
+            hiperligacao = arquivo.readline().strip()
+
+            if not hiperligacao:
+                input("\n                  Clique Enter para fechar")
+                break
+
+
+            print(f"                  Abrindo: {hiperligacao}")
+            try:
+                os.startfile(hiperligacao)
+            except FileNotFoundError:
+                print(f"Erro: Arquivo ou caminho não encontrado: {hiperligacao}")
+            except OSError as e:
+                print(f"Erro do sistema ao tentar abrir {hiperligacao}: {e}")
+            except Exception as e:
+                print(f"Ocorreu um erro ao abrir {hiperligacao}: {e}")
+
+
+
+
+def criar_setup(nome_setup:str):
+    pass
+
+def ler_hiper():
+    txt = r"C:\Users\raulm\Documents\github projects\setup\pythonProject1\hiperligações.txt"
+    nomes = ""
+    hiperligacoes = []
+    quantidade = 0
+    sair = False
+    with open(txt, 'r', encoding='utf-8') as arquivo:
+        while sair != True:
+            nome = arquivo.readline().strip()
+            if not nome:
+                sair = True
+            else:
+                if quantidade == 0:
+                    nomes = nome
+                else:
+                    nomes = nomes + "," + nome
+                hiperligacao = arquivo.readline().strip()
+                hiperligacoes.append(hiperligacao)
+                quantidade = quantidade + 1
+    return nomes,hiperligacoes,quantidade
+
+
+#main
+
+default_text = "Abrir,Adicionar,Remover"
+setups_nomes,setups_hiperligacoes,quantidade_setups = ler_hiper()
 
 print(banner)
-resposta = int(input(info_maker(3,"Setup gaming,Setup programing,Criar setup")))
 
-if(resposta == 1):
-    limpar()
-    resposta = int(input(info_maker(3,"Abrir,Adicionar,Remover")))
-    add_remove(resposta, "a")
+setup_type = int(input(info_maker(quantidade_setups,setups_nomes)))
 
-elif(resposta == 2):
-    limpar()
+limpar()
+resposta = int(input(info_maker(2,"Abrir,Editar")))
+add_remove(resposta,fr"{setups_hiperligacoes[setup_type - 1]}")
 
-    resposta = int(input(info_maker(2,"Default,python")))
-
-    if(resposta == 1):
-        limpar()
-
-        resposta = int(input(info_maker(3,"Abrir,Adicionar,Remover")))
-        add_remove(resposta,"a")
-
-    elif(resposta == 2):
-        limpar()
-
-        resposta = int(input(info_maker(3,"Abrir,Adicionar,Remover")))
-        add_remove(resposta,"a")
 
