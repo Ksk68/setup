0bffdfd15356d3c53a5717a752403e5691afdfdb
Raul Viela <raulvilela16@gmail.com>
2024-09-20 14:17:34 +0100

    aaa
    a

--- pythonProject1/setup - by jkmt.py
+++ pythonProject1/setup - by jkmt.py
@@ -8,7 +8,6 @@ banner = """
     ███████║███████╗   ██║   ╚██████╔╝██║     
     ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     
     """
-
 def info_maker(quantidade:int,palavras:str):
     palavras = palavras.split(",")
     tamanho = ""
@@ -46,7 +45,6 @@ def add_remove(resposta:int,ficheiro:str):
 
 
 def abrir_apps(ficheiro:str):
-
     with open(ficheiro, 'r', encoding='utf-8') as arquivo:
         print(f"\n                  Abrindo arquivo: {ficheiro}")
         while True:
@@ -61,20 +59,45 @@ def abrir_apps(ficheiro:str):
             try:
                 os.startfile(hiperligacao)
             except FileNotFoundError:
-                print(f"Erro: Arquivo ou caminho não encontrado: {hiperligacao}")
+                print(f"                  Erro: Arquivo ou caminho não encontrado: {hiperligacao}")
             except OSError as e:
-                print(f"Erro do sistema ao tentar abrir {hiperligacao}: {e}")
+                print(f"                  Erro do sistema ao tentar abrir {hiperligacao}: {e}")
             except Exception as e:
-                print(f"Ocorreu um erro ao abrir {hiperligacao}: {e}")
+                print(f"                  Ocorreu um erro ao abrir {hiperligacao}: {e}")
 
 
 
 
 def criar_setup(nome_setup:str):
+    caminho_novo = rf"C:\setup\pythonProject1\{nome_setup}.txt"
+    caminho = r"C:\setup\pythonProject1\hiperligações.txt"
+    with open(caminho, 'a', encoding='utf-8') as arquivo:
+        arquivo.write(f"\n{nome_setup}\n")
+        arquivo.write(caminho_novo)
+        with open(caminho_novo, 'w', encoding='utf-8') as arquivo:
+            print("                  Agora insira as hiperligações de cada app")
+            print("                  Enter cada hiperligação troque de linha por exemplo:")
+            print(r"                 C:\Users\nome\Documentos\importante.txt")
+            print(r"                 C:\Users\nome\Fotos")
+            print(r"                 C:\Users\nome\Musica\melhores.txt")
+            arquivo.write(r"""Agora insira as hiperligações de cada app
+Enter cada hiperligação troque de linha por exemplo:
+C:\Users\nome\Documentos\importante.txt
+C:\Users\nome\Fotos
+C:\Users\nome\Musica\melhores.txt
+                             
+**APAGAR TUDO O QUE ESTIVER NO FICHEIRO TXT**
+**APAGAR TUDO O QUE ESTIVER NO FICHEIRO TXT**
+**APAGAR TUDO O QUE ESTIVER NO FICHEIRO TXT**""")
+            add_remove(2,caminho_novo)
+
+
+def remover_setup(nome:str):
     pass
 
+
 def ler_hiper():
-    txt = r"C:\Users\raulm\Documents\github projects\setup\pythonProject1\hiperligações.txt"
+    txt = r"C:\setup\pythonProject1\hiperligações.txt"
     nomes = ""
     hiperligacoes = []
     quantidade = 0
@@ -101,11 +124,38 @@ default_text = "Abrir,Adicionar,Remover"
 setups_nomes,setups_hiperligacoes,quantidade_setups = ler_hiper()
 
 print(banner)
+if setups_nomes == "":
+    setups_nomes = "Criar setup"
+else:
+    setups_nomes = setups_nomes + ",Criar setup,Apagar setup"
 
-setup_type = int(input(info_maker(quantidade_setups,setups_nomes)))
+
+setups_hiperligacoes.append("criar_setup")
+setups_hiperligacoes.append("apagar_setup")
+
+setup_type = int(input(info_maker(quantidade_setups + 1,setups_nomes)))
 
 limpar()
-resposta = int(input(info_maker(2,"Abrir,Editar")))
-add_remove(resposta,fr"{setups_hiperligacoes[setup_type - 1]}")
+if setups_hiperligacoes[setup_type - 1] == "criar_setup":
+    nome = input("\n\nnome para o teu setup:")
+    criar_setup(nome)
+
+elif setups_hiperligacoes[setup_type - 1] == "apagar_setup":
+    setups_nomes = setups_nomes.split(",")
+    setups_nomes.remove("Criar setup")
+    setups_nomes.remove("Apagar setup")
+    print(f"{setups_nomes}")
+    sair = False
+    while sair != True:
+        nome = input("\nnome do setup que deseja apagar:")
+        if nome in setups_nomes:
+            remover_setup(nome)
+        limpar()
+        print("nome invalido")
+
+
+else:
+    resposta = int(input(info_maker(2,"Abrir,Editar")))
+    add_remove(resposta,fr"{setups_hiperligacoes[setup_type - 1]}")
 
 
